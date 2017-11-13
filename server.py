b4fd0305d60394af3ca8d3aebf87b5efe44bec8b
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-11-13 10:11:34 -0800

    Adding additional API call for phone number

--- server.py
+++ server.py
@@ -10,8 +10,6 @@ def display_oncalls():
     """Display on-calls."""
 
     API_KEY = '' # Your API key here
-
-    url = 'https://api.pagerduty.com/oncalls'
     
     headers = {
         'Accept': 'application/vnd.pagerduty+json;version=2',
@@ -23,10 +21,27 @@ def display_oncalls():
     # Just add params=payload to the GET request below like so:
     # r = requests.get(url, headers=headers, params=payload)
     
-    r = requests.get(url, headers=headers)
+    r = requests.get("https://api.pagerduty.com/oncalls", headers=headers)
+
+    oncall_data = {} # Python dict will be structured like: {user_id: [ep_name, user_name, phone_num]}
+
+    for oncall in r.json()['oncalls']:
+        
+        user_id = oncall['user']['id']
+        user_name = oncall['user']['summary']
+        ep_name = oncall['escalation_policy']['summary']
+
+        r = requests.get("https://api.pagerduty.com/users/{}/contact_methods?type=phone_contact_method".format(user_id), headers=headers)
+
+        if r.json()['contact_methods']:
+            phone_num = r.json()['contact_methods'][0]['address']
+        else:
+            phone_num = "No phone number specified"
+
+        oncall_data[user_id] = [ep_name, user_name, phone_num]
 
     # Passing the JSON body to the Jinja template in templates/index.html
-    return render_template("index.html", oncalls=r.json()['oncalls'])
+    return render_template("index.html", oncalls=oncall_data)
 
 
 if __name__ == "__main__":
